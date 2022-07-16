7d0250ebecd8059809e37111706db0333fc03675
Sten Otto Johnsen <sten.johnsen@bouvet.no>
2022-07-16 17:53:36 +0200

    Small update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello Express! are we now working?'));
+app.get('/', (req, res) => res.send('Hello Express! <h1>ARE actually now working!!</h1>'));
 console.log('Hello Express')
 app.listen(5000);
 
