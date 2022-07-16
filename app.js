069b43513a5c81c6ae8f7590f0e2bd47bc6b792e
Sten Otto Johnsen <sten.johnsen@bouvet.no>
2022-07-16 16:40:46 +0200

    Trying again

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello Express! are we now working?'));
+console.log('Hello Express')
 app.listen(80);
 
